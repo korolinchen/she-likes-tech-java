303eb158fb8d4e8bd60fa8882edabd0c82f7a05d
karolinchen-parmas <karolina.parmas@protonmail.ch>
2023-03-03 21:07:12 +0200

    adding two types of data (int) and (literal number)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // literal value just numbers
+        System.out.println("12345");
+        // variable:
+        // holder for data
+        // needs good name eg. myDaughterage
+        //needs type of variable
+        //that data type limits possible set of allowed values
+        // int - integer numbers
+        int myDaugtherAge = 4;
+        // assigning new value
+        myDaugtherAge = 11;
+        // sout + enter
+        System.out.println(myDaugtherAge);
     }
 }
