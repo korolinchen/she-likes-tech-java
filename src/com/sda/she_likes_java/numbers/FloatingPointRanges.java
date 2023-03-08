f99617aabb277c0535d356105afb88e9a5a907a1
karolinchen-parmas <karolina.parmas@protonmail.ch>
2023-03-08 21:05:33 +0200

    floatings numbers ranges

---
+++ src/com/sda/she_likes_java/numbers/FloatingPointRanges.java
@@ -0,0 +1,19 @@
+package com.sda.she_likes_java.numbers;
+
+// names of the class should start with a Capital letter
+// names of the variable should start with lower case letter
+public class FloatingPointRanges {
+    public static void main(String[] args) {
+        System.out.println("Floating numbers...");
+        System.out.println("float number");
+        System.out.println("minimal float number is: " + Float.MIN_VALUE);
+        System.out.println("maximum float number is: " + Float.MAX_VALUE);
+
+        System.out.println();
+
+        System.out.println("double numbers...");
+        System.out.println("minimal double is: " + Double.MIN_VALUE);
+        System.out.println("maximum double number is: " + Double.MAX_VALUE);
+
+    }
+}
