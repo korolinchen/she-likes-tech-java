b025ed9a749e6f7515a0fef2ac5fa8ff49a17850
karolinchen-parmas <karolina.parmas@protonmail.ch>
2023-03-08 20:19:24 +0200

    packages introduced (update)

--- src/com/sda/she_likes_java/homework/FourCalc.java
+++ src/com/sda/she_likes_java/homework/FourCalc.java
@@ -16,17 +16,22 @@ public class FourCalc {
         Scanner inputReader = new Scanner(System.in);
         oranges = inputReader.nextInt();
         System.out.println("Number of oranges you told us about = " + oranges);
-        System.out.println("How many bananas do you have?");
+        System.out.println("...");
+        // the \n means put new line
+        System.out.println("\nHow many bananas do you have?");
         bananas = inputReader.nextInt();
         System.out.println("Number of bananas you told us about = " + bananas);
-        System.out.println("How many kiwis do you have?");
+        System.out.println("...");
+        System.out.println("\nHow many kiwis do you have?");
         kiwis = inputReader.nextInt();
         System.out.println("Number of kiwis you told us about = " + kiwis);
-        System.out.println("How many pineapples do you have?");
+        System.out.println("...");
+        System.out.println("\nHow many pineapples do you have?");
         pineapples = inputReader.nextInt();
         System.out.println("Number of pineapples you told us about = " + pineapples);
+        System.out.println("...");
         // adding fruits in one plate
         int sum = oranges + bananas + kiwis + pineapples;
-        System.out.println("The total number of fruits on your plate now = " + sum);
+        System.out.println("\nThe total number of fruits on your plate now = " + sum);
     }
 }
