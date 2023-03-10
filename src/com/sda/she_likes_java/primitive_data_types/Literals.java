aa9f899be8cfee64ef2201ba7589c0fc171b317a
karolinchen-parmas <karolina.parmas@protonmail.ch>
2023-03-10 21:28:25 +0200

    literals

---
+++ src/com/sda/she_likes_java/primitive_data_types/Literals.java
@@ -0,0 +1,22 @@
+package com.sda.she_likes_java.primitive_data_types;
+
+public class Literals {
+    public static void main(String[] args) {
+        // literal is direct value
+        // String literal is e.g. "name"
+        String name = "Karolinchen";
+
+        // integer literal
+        int age = 33; // just age type of int
+
+        // long literal
+        long anotherAge = 28L;
+
+        // double literal
+        double  doubleNumber = 3.1;
+
+        // floating literal
+        float floatNumber = 3.1F;
+
+    }
+}
