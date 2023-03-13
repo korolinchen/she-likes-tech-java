ab1b26726ea01f9d147f1134de916f194ae57c68
karolinchen-parmas <karolina.parmas@protonmail.ch>
2023-03-13 20:46:47 +0200

    DoWhileLoop

---
+++ src/com/sda/she_likes_java/loops/do_while/DoWhileLoop.java
@@ -0,0 +1,12 @@
+package com.sda.she_likes_java.loops.do_while;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        boolean isAdult = false;
+        // loop with do, will be executed at least once
+        do {
+            System.out.println("Can you see this line?");
+        } while (isAdult);
+
+    }
+}
